b243134e89eebad751fd5fed3e56f610e03cf844
amal <amal.141726@gmail.com>
2021-10-12 06:56:26 +0300

    lab1 day12

---
+++ tas3/CELL.swift
@@ -0,0 +1,13 @@
+//
+//  CELL.swift
+//  tas3
+//
+//  Created by Dr. Fahad on 06/03/1443 AH.
+//
+
+import UIKit
+
+
+class LabelCell : UITableViewCell {
+    @IBOutlet weak var labelCell: UILabel!
+}
