dc7620bff973ea6b1681350e74505957ecaee59d
PC <alahmryn5@gmail.com>
2021-10-14 11:36:59 +0300

    done task 12

---
+++ task1,2,3/EmApp/EmApp/Data.swift
@@ -0,0 +1,25 @@
+//
+//  Email.swift
+//  EmApp
+//
+//  Created by PC on 06/03/1443 AH.
+//
+
+import UIKit
+
+struct Email {
+    
+    let image2: String
+    let nameSender: String
+    let message: String
+    let isfavor: Bool
+    
+}
+
+let email = [Email(image2: "z1", nameSender: "Jack", message: "Hi", isfavor: false),
+             Email(image2: "z2", nameSender: "Rani", message: "Good Morning!", isfavor: false),
+             Email(image2: "z3", nameSender: "Jay", message: "Have a nice day", isfavor: false),
+             Email(image2: "z4", nameSender: "Sa", message: "How are you Jawaher?", isfavor: false),
+             Email(image2: "z5", nameSender: "Fay", message: "Yes", isfavor: false),
+             Email(image2: "z6", nameSender: "Raz", message: "Thank you", isfavor: false)
+]
